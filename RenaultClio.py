4d94bfe385e4621e09fa299e7488f2353d1bc64d
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-12-15 16:38:12 +0100

    first commit

--- RenaultClio.py
+++ RenaultClio.py
@@ -5,16 +5,30 @@ class RenaultClio:
     price=range(8000,30001)
 
     def __init__(self):
-        self._number_doors=None
-        self._color_number=None
-        self._color=None
+        self.__number_doors=None
+        self.__color_number=None
+        self.__color=None
 
     def get_number_doors(self):
-        return self._number_doors
+        return self.__number_doors
 
     def set_number_doors(self,new_number_doors):
         if new_number_doors in RenaultClio.doors:
-            self._number_doors=new_number_doors
+            self.__number_doors=new_number_doors
+
+    def get_color_number(self):
+        return self.__color_number
+
+    def set_color_number(self,new_number_doors):
+        if new_number_doors in RenaultClio.colors.values():
+            self.__color_number=new_number_doors
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self,new_color):
+        if new_color in RenaultClio.colors:
+            self.__color=new_color
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from voiture import Voiture
+from RenaultClio import RenaultClio
 from bus import Bus
+c=RenaultClio()
+print(c.get_number_doors())
+c.set_color("noire")
+print("colors",c.get_color())
 v=Voiture("49-fs-75","red")
 b=Bus("14-sq-59","yellow")
 v.number_dors=3
