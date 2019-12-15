d9e1fd34e577e4e8636ff03f792d0592df8cdd54
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-12-15 16:41:23 +0100

    typo

--- bus.py
+++ bus.py
@@ -6,13 +6,13 @@ class Bus(Vehicule):
         Vehicule.__init__(self,immatriculation,color)
         self.__number_floors=None
 
-        @property
-        def number_floors(self):
+    @property
+    def number_floors(self):
             return self.__number_floors
 
-        @number_floors.setter
-        def number_floors(self, new_value):
-            if new_value in [1, 2]:
-                self.__number_floors = new_value
-            else:
-                raise ValueError("le nombre d'etage doit etre 1 ou 2")
+    @number_floors.setter
+    def number_floors(self, new_value):
+        if new_value in [1, 2]:
+            self.__number_floors = new_value
+        else:
+            raise ValueError("le nombre d'etage doit etre 1 ou 2")
--- main.py
+++ main.py
@@ -8,6 +8,6 @@ print("colors",c.get_color())
 v=Voiture("49-fs-75","red")
 b=Bus("14-sq-59","yellow")
 v.number_dors=3
-b.number_floors=5
+b.number_floors=1
 print(v.number_dors)
 print(b.number_floors)
